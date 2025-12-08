9b35a3ee284d985c1ac87708bdd2fc10e54b2f58
Shayne McGregor Ph.D. <shaynemcgregor1@gmail.com>
2025-12-08 08:37:54 -0500

    Time: 2 ms (41.89%), Space: 55.6 MB (51.43%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function(nums, original) {
+    const seen = new Set(nums);
+
+    
+    while (seen.has(original)) {
+        original *= 2;
+    }
+
+    return original;
+};
